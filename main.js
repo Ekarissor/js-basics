710b0e1f3c0bd06e525f30103d5273ecec5690e3
Your Name <you@example.com>
2019-01-04 11:13:29 +0100

    Camera activation

--- main.js
+++ main.js
@@ -112,16 +112,15 @@ function vibrate (){
 }
 btnvibrate.addEventListener("click", vibrate);
 
+// CAMERA
+var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
 
-// XVIDEO //
-// var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
-
-// navigator.mediaDevices.getUserMedia(constraints)
-// .then(function(mediaStream) {
-//   var video = document.querySelector('video');
-//   video.srcObject = mediaStream;
-//   video.onloadedmetadata = function(e) {
-//     video.play();
-//   };
-// })
-// .catch(function(err) { console.log(err.name + ": " + err.message); });
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  var video = document.querySelector('video');
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); });
